82286ac537d5db7693cbb9c5a6a940ab31be95f3
xun-zi <xun-zi@1228304333@qq.com>
2023-02-15 20:31:07 +0800

    尝试部署

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from "@/store/index"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import "normalize.css"
 import "@/assets/svg/iconfont.css"
 import { GlobalStyle } from './style'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
-      <GlobalStyle/>
+      <GlobalStyle />
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
